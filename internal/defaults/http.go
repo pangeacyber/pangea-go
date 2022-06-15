2f0175128599d2e5ff5cf18cc4cfa7901c88e0d4
alan <alang.gagliero@pangea.cloud>
2022-06-15 14:44:16 -0300

    Add defaults pkg

---
+++ internal/defaults/http.go
@@ -0,0 +1,55 @@
+package defaults
+
+import (
+	"net"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+// HTTPTransport returns a new http.Transport with similar default values to
+// http.DefaultTransport, but with idle connections and keepalives disabled.
+func HTTPTransport() *http.Transport {
+	transport := HTTPPooledTransport()
+	transport.DisableKeepAlives = true
+	transport.MaxIdleConnsPerHost = -1
+	return transport
+}
+
+// HTTPPooledTransport returns a new http.Transport with similar default values to http.DefaultTransport.
+func HTTPPooledTransport() *http.Transport {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		ForceAttemptHTTP2:     true,
+		MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
+	}
+	return transport
+}
+
+// HTTPClient returns a new http.Client with similar default values to
+// http.Client, but with a non-shared Transport, idle connections disabled, and
+// keepalives disabled.
+func HTTPClient() *http.Client {
+	return &http.Client{
+		Transport: HTTPTransport(),
+	}
+}
+
+// HTTPPooledClient returns a new http.Client with similar default values to
+// http.Client, but with a shared Transport. Do not use this function for
+// transient clients as it can leak file descriptors over time. Only use this
+// for clients that will be re-used for the same host(s).
+func HTTPPooledClient() *http.Client {
+	return &http.Client{
+		Transport: HTTPPooledTransport(),
+	}
+}
