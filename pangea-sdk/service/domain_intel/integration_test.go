e141e43a691fc82d6223af07f50ad65509a2f152
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-01-26 19:13:16 -0300

    rename lookup endpoints

--- pangea-sdk/service/domain_intel/integration_test.go
+++ pangea-sdk/service/domain_intel/integration_test.go
@@ -144,3 +144,127 @@ func Test_Integration_DomainLookup_Error_Provider(t *testing.T) {
 	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Code, "NotEnumMember")
 	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Source, "/provider")
 }
+
+// Reputation domain malicious
+func Test_Integration_DomainReputation(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	cfg := intelDomainIntegrationCfg(t)
+	domainintel := domain_intel.New(cfg)
+
+	input := &domain_intel.DomainReputationRequest{
+		Domain:   "737updatesboeing.com",
+		Raw:      true,
+		Verbose:  true,
+		Provider: "domaintools",
+	}
+	out, err := domainintel.Reputation(ctx, input)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+
+	assert.NotNil(t, out)
+	assert.NotNil(t, out.Result.Data)
+	assert.Equal(t, out.Result.Data.Verdict, "malicious")
+}
+
+// Reputation domain unknown
+func Test_Integration_DomainReputation_2(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	cfg := intelDomainIntegrationCfg(t)
+	domainintel := domain_intel.New(cfg)
+
+	input := &domain_intel.DomainReputationRequest{
+		Domain:   "google.com",
+		Raw:      true,
+		Verbose:  true,
+		Provider: "domaintools",
+	}
+
+	out, err := domainintel.Reputation(ctx, input)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+
+	assert.NotNil(t, out)
+	assert.NotNil(t, out.Result.Data)
+	assert.Equal(t, out.Result.Data.Verdict, "benign")
+}
+
+// Test empty domain
+func Test_Integration_DomainReputation_Error(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	cfg := intelDomainIntegrationCfg(t)
+	domainintel := domain_intel.New(cfg)
+
+	input := &domain_intel.DomainReputationRequest{
+		Domain:   "",
+		Raw:      true,
+		Verbose:  true,
+		Provider: "domaintools",
+	}
+
+	out, err := domainintel.Reputation(ctx, input)
+
+	assert.Error(t, err)
+	assert.Nil(t, out)
+	err = err.(*pangea.APIError)
+	apiErr := err.(*pangea.APIError)
+	assert.Equal(t, len(apiErr.PangeaErrors.Errors), 1)
+	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Code, "BadFormatHostname")
+	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Detail, "'domain' must be a valid RFC1123 hostname")
+	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Source, "/domain")
+}
+
+// Bad auth token
+func Test_Integration_DomainReputation_Error_BadAuthToken(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	cfg := intelDomainIntegrationCfg(t)
+	cfg.Token = "notavalidtoken"
+	domainintel := domain_intel.New(cfg)
+
+	input := &domain_intel.DomainReputationRequest{
+		Domain:   "737updatesboeing.com",
+		Raw:      true,
+		Verbose:  true,
+		Provider: "domaintools",
+	}
+	out, err := domainintel.Reputation(ctx, input)
+
+	assert.Error(t, err)
+	assert.Nil(t, out)
+	apiErr := err.(*pangea.APIError)
+	assert.Equal(t, apiErr.Err.Error(), "API error: Not authorized to access this resource.")
+}
+
+// Not valid provider
+func Test_Integration_DomainReputation_Error_Provider(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFn()
+
+	cfg := intelDomainIntegrationCfg(t)
+	domainintel := domain_intel.New(cfg)
+
+	input := &domain_intel.DomainReputationRequest{
+		Domain:   "737updatesboeing.com",
+		Raw:      true,
+		Verbose:  true,
+		Provider: "notaprovider",
+	}
+	out, err := domainintel.Reputation(ctx, input)
+
+	assert.Error(t, err)
+	assert.Nil(t, out)
+	err = err.(*pangea.APIError)
+	apiErr := err.(*pangea.APIError)
+	assert.Equal(t, len(apiErr.PangeaErrors.Errors), 1)
+	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Code, "NotEnumMember")
+	assert.Equal(t, apiErr.PangeaErrors.Errors[0].Source, "/provider")
+}
--- pangea-sdk/service/file_intel/api.go
+++ pangea-sdk/service/file_intel/api.go
@@ -11,6 +11,7 @@ import (
 )
 
 type FileLookupInput struct {
+	// Deprecated: Use Reputation instead.
 	Hash string `json:"hash"`
 
 	// One of "sha256", "sha", "md5".
@@ -27,6 +28,7 @@ type FileLookupInput struct {
 }
 
 type LookupData struct {
+	// Deprecated: Use Reputation instead.
 	// The categories that apply to this
 	// indicator as determined by the provider
 	Category []string `json:"category"`
@@ -57,6 +59,8 @@ type FileLookupOutput struct {
 // Look up a file
 //
 // Lookup a file's hash to retrieve reputation data.
+//
+// Deprecated: Use Reputation instead.
 //
 // Example:
 //
@@ -89,8 +93,9 @@ func (e *FileIntel) Lookup(ctx context.Context, input *FileLookupInput) (*pangea
 	return &panresp, err
 }
 
-// Create a FileLookupInput from path file
+// Create a FileReputationRequest from path file
 func NewFileLookupInputFromFilepath(fp string) (*FileLookupInput, error) {
+	// Deprecated: Use NewFileReputationRequestFromFilepath instead.
 	f, err := os.Open(fp)
 	if err != nil {
 		return nil, err
@@ -103,6 +108,104 @@ func NewFileLookupInputFromFilepath(fp string) (*FileLookupInput, error) {
 	}
 
 	return &FileLookupInput{
+		Hash:     hex.EncodeToString(h.Sum(nil)),
+		HashType: "sha256",
+	}, nil
+}
+
+type FileReputationRequest struct {
+	Hash string `json:"hash"`
+
+	// One of "sha256", "sha", "md5".
+	HashType string `json:"hash_type"`
+
+	// Echo the API parameters in the response.
+	Verbose bool `json:"verbose,omitempty"`
+
+	// Include raw data from this provider.
+	Raw bool `json:"raw,omitempty"`
+
+	// Use reputation data from this provider.
+	Provider string `json:"provider,omitempty"`
+}
+
+type ReputationData struct {
+	// The categories that apply to this
+	// indicator as determined by the provider
+	Category []string `json:"category"`
+
+	// The score, given by the Pangea service,
+	// for the indicator
+	Score int `json:"score"`
+
+	// The verdict, given by the Pangea service,
+	// for the indicator
+	Verdict string `json:"verdict"`
+}
+
+type FileReputationResult struct {
+	// High-level normalized results sent
+	// by the Pangea service
+	Data ReputationData `json:"data"`
+
+	// The parameters, which were passed in
+	// the request, echoed back
+	Parameters interface{} `json:"parameters,omitempty"`
+
+	// The raw data from the provider.
+	// Each provider's data will have its own format
+	RawData interface{} `json:"raw_data,omitempty"`
+}
+
+// Look up a file
+//
+// Lookup a file's hash to retrieve reputation data.
+//
+// Example:
+//
+//	input := &file_intel.FileReputationRequest{
+//	    Hash: "322ccbd42b7e4fd3a9d0167ca2fa9f6483d9691364c431625f1df54270647ca8",
+//	    HashType: "sha256",
+//	    Raw: true,
+//	    Verbose: true,
+//	    Provider: "reversinglabs",
+//	}
+//
+//	checkOutput, _, err := fileintel.Reputation(ctx, input)
+func (e *FileIntel) Reputation(ctx context.Context, input *FileReputationRequest) (*pangea.PangeaResponse[FileReputationResult], error) {
+	req, err := e.Client.NewRequest("POST", "v1/lookup", input)
+	if err != nil {
+		return nil, err
+	}
+	out := FileReputationResult{}
+	resp, err := e.Client.Do(ctx, req, &out)
+
+	if resp == nil {
+		return nil, err
+	}
+
+	panresp := pangea.PangeaResponse[FileReputationResult]{
+		Response: *resp,
+		Result:   &out,
+	}
+
+	return &panresp, err
+}
+
+// Create a FileReputationRequest from path file
+func NewFileReputationRequestFromFilepath(fp string) (*FileReputationRequest, error) {
+	f, err := os.Open(fp)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return nil, err
+	}
+
+	return &FileReputationRequest{
 		Hash:     hex.EncodeToString(h.Sum(nil)),
 		HashType: "sha256",
 	}, nil
