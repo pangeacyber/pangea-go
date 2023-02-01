77231b060680bafa170e769e124f8eef612bc671
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-02-01 12:07:35 -0300

    add reputation to interface

--- pangea-sdk/service/ip_intel/service.go
+++ pangea-sdk/service/ip_intel/service.go
@@ -8,6 +8,7 @@ import (
 
 type Client interface {
 	Lookup(ctx context.Context, input *IpLookupRequest) (*pangea.PangeaResponse[IpLookupResult], error)
+	Reputation(ctx context.Context, input *IpReputationRequest) (*pangea.PangeaResponse[IpReputationResult], error)
 	Geolocate(ctx context.Context, input *IpGeolocateRequest) (*pangea.PangeaResponse[IpGeolocateResult], error)
 	GetDomain(ctx context.Context, input *IpDomainRequest) (*pangea.PangeaResponse[IpDomainResult], error)
 	IsVPN(ctx context.Context, input *IpVPNRequest) (*pangea.PangeaResponse[IpVPNResult], error)
