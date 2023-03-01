428ad13f1f4e7ca3239e46b1fa2f53b87309727a
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2023-03-01 15:12:49 -0300

    GEA 4804 release DE (#46)
    Add /geolocate, /domain, /vpn and /proxy support

--- pangea-sdk/service/ip_intel/service.go
+++ pangea-sdk/service/ip_intel/service.go
@@ -8,13 +8,18 @@ import (
 
 type Client interface {
 	Lookup(ctx context.Context, input *IpLookupRequest) (*pangea.PangeaResponse[IpLookupResult], error)
+	Reputation(ctx context.Context, input *IpReputationRequest) (*pangea.PangeaResponse[IpReputationResult], error)
+	Geolocate(ctx context.Context, input *IpGeolocateRequest) (*pangea.PangeaResponse[IpGeolocateResult], error)
+	GetDomain(ctx context.Context, input *IpDomainRequest) (*pangea.PangeaResponse[IpDomainResult], error)
+	IsVPN(ctx context.Context, input *IpVPNRequest) (*pangea.PangeaResponse[IpVPNResult], error)
+	IsProxy(ctx context.Context, input *IpProxyRequest) (*pangea.PangeaResponse[IpProxyResult], error)
 }
 
 type IpIntel struct {
 	*pangea.Client
 }
 
-func New(cfg *pangea.Config) *IpIntel {
+func New(cfg *pangea.Config) Client {
 	cli := &IpIntel{
 		Client: pangea.NewClient("ip-intel", cfg),
 	}
