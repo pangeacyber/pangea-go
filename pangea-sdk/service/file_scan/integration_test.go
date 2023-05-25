6c55065e800ac97cde3835406cf6341916d98d35
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-05-25 15:32:30 -0300

    test against dev

--- pangea-sdk/service/file_scan/integration_test.go
+++ pangea-sdk/service/file_scan/integration_test.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	testingEnvironment = pangeatesting.Live
+	testingEnvironment = pangeatesting.Develop
 	EICAR              = "X5O!P%@AP[4\\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*\n"
 )
 
