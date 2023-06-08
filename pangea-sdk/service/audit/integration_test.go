a8ce3d6db3ba6f78c8e4c91e7207d49428c69365
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2023-06-08 16:14:55 -0300

    GEA 5011 improve test coverage (#91)
    * utils tests
    * test verifier old format
    * add services tests
    * pangea tests

--- pangea-sdk/service/audit/integration_test.go
+++ pangea-sdk/service/audit/integration_test.go
@@ -441,6 +441,7 @@ func Test_Integration_Log_Error_BadAuthToken(t *testing.T) {
 	assert.Nil(t, out)
 	apiErr := err.(*pangea.APIError)
 	assert.Equal(t, apiErr.Err.Error(), "API error: Not authorized to access this resource.")
+	assert.NotEmpty(t, apiErr.Error())
 }
 
 // Fails because empty message
