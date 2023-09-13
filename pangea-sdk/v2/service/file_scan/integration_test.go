63fd389ab83d20118f69e91540303bee62a4f883
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-09-13 16:46:20 -0300

    add reversinglabs tests

--- pangea-sdk/v2/service/file_scan/integration_test.go
+++ pangea-sdk/v2/service/file_scan/integration_test.go
@@ -18,7 +18,7 @@ const (
 	TESTFILE_PATH      = "./testdata/testfile.pdf"
 )
 
-func Test_Integration_FileScan(t *testing.T) {
+func Test_Integration_FileScan_crowdstrike(t *testing.T) {
 	ctx, cancelFn := context.WithTimeout(context.Background(), 600*time.Second)
 	defer cancelFn()
 
@@ -48,7 +48,7 @@ func Test_Integration_FileScan(t *testing.T) {
 	assert.Equal(t, resp.Result.Data.Verdict, "benign")
 }
 
-func Test_Integration_FileScan_NoRetry(t *testing.T) {
+func Test_Integration_FileScan_NoRetry_crowdstrike(t *testing.T) {
 	ctx, cancelFn := context.WithTimeout(context.Background(), 120*time.Second)
 	defer cancelFn()
 
@@ -60,6 +60,72 @@ func Test_Integration_FileScan_NoRetry(t *testing.T) {
 		Raw:      true,
 		Verbose:  true,
 		Provider: "crowdstrike",
+	}
+
+	file, err := os.Open(TESTFILE_PATH)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+
+	resp, err := client.Scan(ctx, input, file)
+	assert.Error(t, err)
+	assert.Nil(t, resp)
+	ae := err.(*pangea.AcceptedError)
+
+	// Wait until result should be ready
+	time.Sleep(time.Duration(10 * time.Second))
+
+	pr, err := client.PollResultByError(ctx, *ae)
+	assert.NoError(t, err)
+	assert.NotNil(t, pr)
+	assert.NotNil(t, pr.Result)
+
+	r := (*pr.Result).(*file_scan.FileScanResult)
+	assert.Equal(t, r.Data.Verdict, "benign")
+}
+
+func Test_Integration_FileScan_reversinglabs(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 600*time.Second)
+	defer cancelFn()
+
+	cfg := pangeatesting.IntegrationConfig(t, testingEnvironment)
+	cfg.PollResultTimeout = 60 * time.Second
+	client := file_scan.New(cfg)
+
+	input := &file_scan.FileScanRequest{
+		Raw:      true,
+		Verbose:  true,
+		Provider: "reversinglabs",
+	}
+
+	file, err := os.Open(TESTFILE_PATH)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err)
+	}
+
+	resp, err := client.Scan(ctx, input, file)
+	if err != nil {
+		t.Fatalf("expected no error got: %v", err.Error())
+	}
+
+	assert.NotNil(t, resp)
+	assert.NotNil(t, resp.Result)
+	assert.NotNil(t, resp.Result.Data)
+	assert.Equal(t, resp.Result.Data.Verdict, "benign")
+}
+
+func Test_Integration_FileScan_NoRetry_reversinglabs(t *testing.T) {
+	ctx, cancelFn := context.WithTimeout(context.Background(), 120*time.Second)
+	defer cancelFn()
+
+	cfg := pangeatesting.IntegrationConfig(t, testingEnvironment)
+	cfg.QueuedRetryEnabled = false
+	client := file_scan.New(cfg)
+
+	input := &file_scan.FileScanRequest{
+		Raw:      true,
+		Verbose:  true,
+		Provider: "reversinglabs",
 	}
 
 	file, err := os.Open(TESTFILE_PATH)
