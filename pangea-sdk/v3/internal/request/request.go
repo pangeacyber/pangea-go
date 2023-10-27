a53fb461061fe1b2230c3b0f6564a5ba68f9a0ca
H. Andres Tournour <andres.tournour@pangea.cloud>
2023-10-27 10:24:55 -0300

    presigned url support

--- pangea-sdk/v3/internal/request/request.go
+++ pangea-sdk/v3/internal/request/request.go
@@ -3,11 +3,12 @@ package request
 import (
 	"context"
 	"errors"
+	"io"
 
 	"github.com/pangeacyber/pangea-go/pangea-sdk/v3/pangea"
 )
 
-func DoPost[T any](ctx context.Context, c *pangea.Client, urlStr string, input pangea.ConfigIDer, out *T) (*pangea.PangeaResponse[T], error) {
+func DoPost[T any](ctx context.Context, c *pangea.Client, path string, input pangea.ConfigIDer, out *T) (*pangea.PangeaResponse[T], error) {
 	if input == nil || out == nil {
 		return nil, errors.New("nil pointer to struct")
 	}
@@ -16,11 +17,51 @@ func DoPost[T any](ctx context.Context, c *pangea.Client, urlStr string, input p
 		return nil, errors.New("nil client")
 	}
 
-	req, err := c.NewRequest("POST", urlStr, input)
+	url, err := c.GetURL(path)
+	if err != nil {
+		c.Logger.Error().
+			Str("service", c.ServiceName()).
+			Str("method", "DoPost").
+			Err(err)
+		return nil, err
+	}
+
+	req, err := c.NewRequest("POST", url, input)
 	if err != nil {
 		return nil, err
 	}
 	resp, err := c.Do(ctx, req, out)
+	if err != nil {
+		return nil, err
+	}
+
+	panresp := pangea.PangeaResponse[T]{
+		Response: *resp,
+		Result:   out,
+	}
+	return &panresp, nil
+}
+
+func DoPostWithFile[T any](ctx context.Context, c *pangea.Client, path string, input any, out *T, file io.Reader) (*pangea.PangeaResponse[T], error) {
+	url, err := c.GetURL(path)
+	if err != nil {
+		c.Logger.Error().
+			Str("service", c.ServiceName()).
+			Str("method", "DoPost").
+			Err(err)
+		return nil, err
+	}
+
+	var resp *pangea.Response
+	err = nil
+	v, ok := input.(pangea.TransferRequester)
+
+	if ok && v.GetTransferMethod() == pangea.TMdirect { // Check TrasnferMethod
+		resp, err = c.PostPresignedURL(ctx, url, input, out, file)
+	} else {
+		resp, err = c.PostMultipart(ctx, url, input, out, file)
+	}
+
 	if err != nil {
 		return nil, err
 	}
