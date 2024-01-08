a4f0cf8a1de54323bd3724f7a6bf733fdaccdc55
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-01-08 15:13:08 -0300

    update examples and add script to run them all

---
+++ dev/run_examples.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# This script is to run all examples at once in order to check them
+# Need to run this script from `./examples/` folder
+
+# Root directory
+root_directory=$(pwd)
+
+# Iterate over subdirectories
+find "$root_directory" -type d -print0 | while IFS= read -r -d '' directory; do
+    echo -e "\n\nChecking $directory..."
+
+    # Check if the directory contains a .mod file
+    if [ -f "$directory/go.mod" ]; then
+        echo -e "Running go mod tidy..."
+        cd "$directory"
+        go mod tidy
+
+        # Run all .go files in the directory and its subdirectories
+        echo -e "Running go files:"
+        find "$directory" -type f -name '*.go' | while read -r file; do
+            echo -e "\n----------------------------------------------------------------------\nRunning $file"
+            go run "$file"
+            echo -e "Finish...\n----------------------------------------------------------------------"
+        done
+
+        cd "$root_directory" || exit
+    fi
+done
