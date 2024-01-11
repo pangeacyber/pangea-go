0e76db3ad2fb8d21e283638fcade430037d835c0
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2024-01-11 13:10:21 -0500

    Add structured encrypt example (GEA-11385)

---
+++ examples/vault/structured_encrypt.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/pangeacyber/pangea-go/pangea-sdk/v3/pangea"
+	"github.com/pangeacyber/pangea-go/pangea-sdk/v3/service/vault"
+)
+
+func main() {
+	token := os.Getenv("PANGEA_VAULT_TOKEN")
+	if token == "" {
+		log.Fatal("missing PANGEA_VAULT_TOKEN environment variable")
+	}
+
+	domain := os.Getenv("PANGEA_DOMAIN")
+	if domain == "" {
+		log.Fatal("missing PANGEA_DOMAIN environment variable")
+	}
+
+	vaultClient := vault.New(&pangea.Config{
+		Token:  token,
+		Domain: domain,
+	})
+
+	ctx := context.Background()
+
+	// First create an encryption key, either from the Pangea Console or
+	// programmatically as below.
+	generateInput := &vault.SymmetricGenerateRequest{
+		Algorithm: vault.SYAaes256_cfb,
+		Purpose:   vault.KPencryption,
+		CommonGenerateRequest: vault.CommonGenerateRequest{
+			Name: "any unique name",
+		},
+	}
+	generateResponse, err := vaultClient.SymmetricGenerate(ctx, generateInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	encryptionKeyId := generateResponse.Result.ID
+
+	// Structured data that we'll encrypt.
+	data := map[string]interface{}{
+		"foo":  [4]interface{}{1, 2, "true", "false"},
+		"some": "thing",
+	}
+
+	encryptInput := &vault.EncryptStructuredRequest{
+		ID:             encryptionKeyId,
+		StructuredData: data,
+		Filter:         "$.foo[2:4]",
+	}
+	encryptResponse, err := vaultClient.EncryptStructured(ctx, encryptInput)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Encrypted result:", pangea.Stringify(encryptResponse.Result.StructuredData))
+}
