2477d2373cc0f10cc91980ea4506c5249c6aee25
H. Andres Tournour <andres.tournour@pangea.cloud>
2024-02-05 16:49:07 -0300

    add skip_items list

--- dev/run_examples.sh
+++ dev/run_examples.sh
@@ -4,6 +4,7 @@
 # Need to run this script from `./examples/` folder
 
 set -e
+skip_items=("/util/")
 
 # Root directory
 root_directory=$(pwd)
@@ -21,9 +22,23 @@ find "$root_directory" -type d -print0 | while IFS= read -r -d '' directory; do
         # Run all .go files in the directory and its subdirectories
         echo -e "Running go files:"
         find "$directory" -type f -name '*.go' | while read -r file; do
-            echo -e "\n----------------------------------------------------------------------\nRunning $file"
-            go run "$file"
-            echo -e "Finish...\n----------------------------------------------------------------------"
+            # Check if the file or directory should be skipped
+            skip=false
+            echo -e "Checking $file"
+            for skip_item in "${skip_items[@]}"; do
+                if [[ "$file" == *"$skip_item"* ]]; then
+                    echo "Skipping $file"
+                    skip=true
+                    break
+                fi
+            done
+
+            # Run the file if it should not be skipped
+            if [ "$skip" = false ]; then
+                echo -e "\n----------------------------------------------------------------------\nRunning $file"
+                go run "$file"
+                echo -e "Finish...\n----------------------------------------------------------------------"
+            fi
         done
 
         cd "$root_directory" || exit
