2d5be221388216f2710a4cc3f34ef1f0eb56fb5d
H. Andrés Tournour <111303288+pangea-andrest@users.noreply.github.com>
2024-03-18 18:10:18 -0300

    Add SOS support (#155)
    Co-authored-by: David Wayman <mrdavidwayman@gmail.com>
    Co-authored-by: Kenan Yildirim <kenan.yildirim@pangea.cloud>

---
+++ examples/share/put_transfer_method_multipart.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/pangeacyber/pangea-go/pangea-sdk/v4/pangea"
+	"github.com/pangeacyber/pangea-go/pangea-sdk/v4/service/share"
+)
+
+func main() {
+	var t = time.Now().Format("20060102_150405")
+	var name = "file_name_" + t
+	const filePath = "./testdata/testfile.pdf"
+
+	// Load pangea token from environment variables
+	token := os.Getenv("PANGEA_SHARE_TOKEN")
+	if token == "" {
+		log.Fatal("Unauthorized: No token present.")
+	}
+
+	ctx, cancelFn := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancelFn()
+
+	// create a new store client with pangea token and domain
+	client := share.New(&pangea.Config{
+		Token:              token,
+		Domain:             os.Getenv("PANGEA_DOMAIN"),
+		QueuedRetryEnabled: true,
+		PollResultTimeout:  60 * time.Second,
+	})
+
+	// Create a PutRequest. In this case TransferMethod is set to TMpostURL
+	// So SDK is going to request a post url, upload the file to that url and then request to pangea for the /put result
+	input := &share.PutRequest{
+		Name: name,
+		TransferRequest: pangea.TransferRequest{
+			TransferMethod: pangea.TMmultipart,
+		},
+	}
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	rPut, err := client.Put(ctx, input, file)
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+
+	fmt.Println("File uploaded:")
+	fmt.Printf("\tID: %s\n", rPut.Result.Object.ID)
+	fmt.Printf("\tName: %s\n", rPut.Result.Object.Name)
+}
---
+++ pangea-sdk/service/share/service.go
@@ -0,0 +1,38 @@
+package share
+
+import (
+	"context"
+	"os"
+
+	"github.com/pangeacyber/pangea-go/pangea-sdk/v4/pangea"
+)
+
+type Client interface {
+	FolderCreate(ctx context.Context, input *FolderCreateRequest) (*pangea.PangeaResponse[FolderCreateResult], error)
+	Delete(ctx context.Context, input *DeleteRequest) (*pangea.PangeaResponse[DeleteResult], error)
+	Get(ctx context.Context, input *GetRequest) (*pangea.PangeaResponse[GetResult], error)
+	Put(ctx context.Context, input *PutRequest, file *os.File) (*pangea.PangeaResponse[PutResult], error)
+	Update(ctx context.Context, input *UpdateRequest) (*pangea.PangeaResponse[UpdateResult], error)
+	List(ctx context.Context, input *ListRequest) (*pangea.PangeaResponse[ListResult], error)
+	GetArchive(ctx context.Context, input *GetArchiveRequest) (*pangea.PangeaResponse[GetArchiveResult], error)
+	ShareLinkCreate(ctx context.Context, input *ShareLinkCreateRequest) (*pangea.PangeaResponse[ShareLinkCreateResult], error)
+	ShareLinkGet(ctx context.Context, input *ShareLinkGetRequest) (*pangea.PangeaResponse[ShareLinkGetResult], error)
+	ShareLinkList(ctx context.Context, input *ShareLinkListRequest) (*pangea.PangeaResponse[ShareLinkListResult], error)
+	ShareLinkDelete(ctx context.Context, input *ShareLinkDeleteRequest) (*pangea.PangeaResponse[ShareLinkDeleteResult], error)
+	ShareLinkSend(ctx context.Context, input *ShareLinkSendRequest) (*pangea.PangeaResponse[ShareLinkSendResult], error)
+	RequestUploadURL(ctx context.Context, input *PutRequest) (*pangea.PangeaResponse[PutResult], error)
+
+	// Base service methods
+	pangea.BaseServicer
+}
+
+type share struct {
+	pangea.BaseService
+}
+
+func New(cfg *pangea.Config) Client {
+	cli := &share{
+		BaseService: pangea.NewBaseService("share", cfg),
+	}
+	return cli
+}
