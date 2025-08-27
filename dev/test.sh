aa1a2cb95b21740442af85fc88bfb8b713618f0a
Kenan Yildirim <kenan.yildirim@pangea.cloud>
2025-08-27 16:08:53 -0400

    Add ability to only send relevant content to AI Guard

--- dev/test.sh
+++ dev/test.sh
@@ -4,7 +4,7 @@ set -e
 
 cd -- "$(dirname -- "$0")/.."
 
-npx start-server-and-test --expect 404 \
-  "npx @stoplight/prism-cli mock -d --json-schema-faker-fillProperties=false dev/specs/ai-guard.openapi.json" \
+npx -y start-server-and-test --expect 404 \
+  "npx -y @stoplight/prism-cli mock -d --json-schema-faker-fillProperties=false dev/specs/ai-guard.openapi.json" \
   4010 \
   "go test ./pangea-sdk/service/ai_guard/mock_test.go"
